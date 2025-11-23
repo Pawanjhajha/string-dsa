b0656c5f29b821c35d6a1e39866a5591793cdcaa
pawan kumar <pawankumarjhajharia3@gmail.com>
2025-11-23 22:01:12 +0530

    longest plindrom

---
+++ longestplindrom.js
@@ -0,0 +1,17 @@
+
+let str = "cbbd";
+//ans bb
+let longest = "";
+
+for (let i = 0; i < str.length; i++) {
+    let sum = ""; // reset sum for each new starting point
+    for (let j = i; j < str.length; j++) {
+        sum += str[j];
+        if (sum === sum.split('').reverse().join('')) {
+            if (sum.length > longest.length) {
+                longest = sum;
+            }
+        }
+    }
+}
+console.log(longest)
