eb9a6e4e4addd985ec79283b095c490f098d8c07
pawan kumar <pawankumarjhajharia3@gmail.com>
2025-11-24 21:17:57 +0530

    longoest

---
+++ longestCommonsubstring.js
@@ -0,0 +1,16 @@
+let strs = ["flower", "flow", "flight"];
+
+if (strs.length === 0) console.log("");
+
+let prefix = strs[0];
+
+for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+        prefix = prefix.slice(0, -1);
+        if (prefix === "") {
+            console.log("");
+        }
+    }
+}
+
+console.log(prefix);   // "fl"
